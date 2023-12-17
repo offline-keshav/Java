3ff2f1d37a248cb2ad6d3434689230d5c409c210
Keshav Kushwaha <keshavkushawaha@gmail.com>
2023-12-17 18:38:17 +0530

    odd even code updated

---
+++ 8 Conditional Statement/2 Odd or Even/code.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class code {
+    public static void main(String aeg[]){
+        System.out.print("Enter number: ");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if (a%2 ==0){
+            System.out.println("number is an even number");
+        } else {
+            System.out.println("Number is an odd");
+        }
+    }
+}
