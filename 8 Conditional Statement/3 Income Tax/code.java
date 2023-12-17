6adc60b3a1a25d0d6db8e9fe2dc130c47aaf6720
Keshav Kushwaha <keshavkushawaha@gmail.com>
2023-12-18 00:47:46 +0530

    Income tax calculator

---
+++ 8 Conditional Statement/3 Income Tax/code.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class code {
+    public static void main ( String arg[]){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter the income:");
+        int income= sc.nextInt();
+        if (income<500000) {
+            System.out.println("Tax aplicable is o percent. Tax = 0");
+        } else {
+            if (income>=500000 && income<1000000) {
+                int tax = income*5/100;
+                System.out.println("Tax applicable is 5 percent. Tax = "+tax);
+            } else {
+                int tax = income*10/100;
+                System.out.println("Tax applicable is 10 percent. Tax = "+tax);
+
+            }
+        }
+    }
+    
+}
